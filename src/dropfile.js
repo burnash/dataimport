f4dbfcd1bf7c8ab923fc92eb993bbb7af788fae7
burnash <anton.burnashev@gmail.com>
2015-03-07 17:38:32 +0300

    Initial commit

---
+++ src/dropfile.js
@@ -0,0 +1,48 @@
+var DropFile = function DropFile(opts) {
+
+    function handleDrop(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        var files = e.dataTransfer.files;
+        var f = files[0];
+        // parse first file
+        //       debugger;
+        function complete(results, file) {
+            if (results.errors) {
+                console.log(results.errors);
+            }
+            opts.on.drawsheet(results.data, results.meta.fields);
+        }
+        Papa.parse(f, {
+            header: true,
+            complete: complete
+        });
+        // draw
+    }
+
+    function handleDragover(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'copy';
+        setTimeout(function() {
+            $(e.target).css('border-color', '#0000ff');
+        }, 10);
+    }
+
+    function handleDragleave(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        setTimeout(function() {
+            $(e.target).css('border-color', '#bbb');
+        }, 10);
+    }
+
+    if (opts.drop.addEventListener) {
+        opts.drop.addEventListener('dragenter', handleDragover, false);
+        opts.drop.addEventListener('dragover', handleDragover, false);
+        opts.drop.addEventListener('dragleave', handleDragleave, false);
+        opts.drop.addEventListener('drop', handleDrop, false);
+    }
+};
+
+module.exports = DropFile;
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    entry: "./csvassets/main.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
